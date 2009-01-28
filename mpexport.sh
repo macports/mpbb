4b8f114ace8d2479c9e407f5cea9f640435e8430
Caspar Florian Ebeling <febeling@macports.org>
2009-01-28 23:20:36 +0000

    mpsexport.sh: script for fetching macports tar ball.
    git-svn-id: https://svn.macports.org/repository/macports/contrib/mpab@46086 d073be05-634f-4543-b044-5fe20cf6d1d6

---
+++ mpexport.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+svn checkout -r HEAD http://svn.macports.org/repository/macports/trunk mpexport
+cd mpexport
+tar c --exclude '.svn' -f - . | bzip2 -c > ../macports_dist.tar.bz2
+cd ..
+#/bin/sh
+svn checkout -r HEAD http://svn.macports.org/repository/macports/trunk mpexport
+cd mpexport
+tar c --exclude '.svn' -f - . | bzip2 -c > ../macports_dist.tar.bz2
+cd ..
