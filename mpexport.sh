c29f5f94695581f8c1ca757173878d5395a1da61
Caspar Florian Ebeling <febeling@macports.org>
2009-01-28 23:29:10 +0000

    remove patch 'artifacts'
    git-svn-id: https://svn.macports.org/repository/macports/contrib/mpab@46090 d073be05-634f-4543-b044-5fe20cf6d1d6

--- mpexport.sh
+++ mpexport.sh
@@ -3,8 +3,3 @@ svn checkout -r HEAD http://svn.macports.org/repository/macports/trunk mpexport
 cd mpexport
 tar c --exclude '.svn' -f - . | bzip2 -c > ../macports_dist.tar.bz2
 cd ..
-#/bin/sh
-svn checkout -r HEAD http://svn.macports.org/repository/macports/trunk mpexport
-cd mpexport
-tar c --exclude '.svn' -f - . | bzip2 -c > ../macports_dist.tar.bz2
-cd ..
