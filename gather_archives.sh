e846cfc37036d16e5bedff8b1caa9525440af3c6
Joshua Root <jmr@macports.org>
2011-08-05 17:15:13 +0000

    mpab: have gather_archives.sh look for logs in their tmp location as well, in case mpab didn't finish cleanly
    git-svn-id: https://svn.macports.org/repository/macports/contrib/mpab@81829 d073be05-634f-4543-b044-5fe20cf6d1d6

--- gather_archives.sh
+++ gather_archives.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+CHROOTSUBDIR=mpchroot
+dataDir=$(pwd)
+if [[ -n "$MPAB_DATA" ]]; then
+   dataDir=$MPAB_DATA
+fi
+chrootPath="${dataDir}/${CHROOTSUBDIR}"
+OSMajor=`uname -r | sed 's/\..*//'`
+# xcodebuild breaks in chroots on 10.6
+if [[ $1 = "-n" || $OSMajor -ge 10 ]]; then
+    chrootPath=""
+    if [[ $1 = "-n" ]]; then
+        shift
+    fi
+fi
+
 if [[ -z "$PORTLISTFILE" ]]; then
     PORTLISTFILE=portlist
 fi
@@ -18,6 +33,16 @@ if [[ `head -n1 $PORTLISTFILE` == "all" ]]; then
     ports=`${PREFIX}/bin/port -q echo all | tr '\n' ' '`
 else
     ports=`cat $PORTLISTFILE`
+fi
+
+# if mpab was killed due to a timeout, logs will still be in the tmp dir
+if ! ls logs-* > /dev/null 2>&1 ; then
+    now=`date '+%Y%m%d-%H%M%S'`
+    baseDir=$(dirname $0)
+    mkdir ${baseDir}/logs-${now}
+    mv ${chrootPath}/var/tmp/portresults/fail ${baseDir}/logs-${now}
+    mv ${chrootPath}/var/tmp/portresults/success ${baseDir}/logs-${now}
+    chmod -R a+rX ${baseDir}/logs-${now}
 fi
 
 for portname in $ports; do
