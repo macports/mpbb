1392c2706a749a1dc73673c228039f7558e21e6d
Ryan Schmidt <git@ryandesign.com>
2019-06-01 15:39:45 -0500

    Uninstall ports without dependents
    See: https://trac.macports.org/ticket/57464

--- tools/uninstall-old-ports.tcl
+++ tools/uninstall-old-ports.tcl
@@ -39,6 +39,42 @@ if {$showVersion} {
     exit 0
 }
 
+# Create a lookup table for determining whether a port has dependents
+# (regardless of whether or not those dependents are currently installed)
+foreach source $macports::sources {
+    set source [lindex $source 0]
+    try -pass_signal {
+        set fd [open [macports::getindex $source] r]
+
+        try -pass_signal {
+            while {[gets $fd line] >= 0} {
+                array unset portinfo
+                set name [lindex $line 0]
+                set len  [lindex $line 1]
+                set line [read $fd $len]
+                array set portinfo $line
+
+                # depends_test is not included because mpbb doesn't run `port test'
+                foreach field {depends_build depends_extract depends_fetch depends_lib depends_patch depends_run} {
+                    if [info exists portinfo($field)] {
+                        foreach dependency $portinfo($field) {
+                            set dependency_name [lindex [split $dependency {:}] end]
+                            incr dependents([string tolower $dependency_name])
+                        }
+                    }
+                }
+            }
+        } catch {*} {
+            ui_warn "It looks like your PortIndex file for $source may be corrupt."
+            throw
+        } finally {
+            close $fd
+        }
+    } catch {*} {
+        ui_warn "Can't open index file for source: $source"
+    }
+}
+
 foreach port [registry::entry imaged] {
     # Set to yes if a port should be uninstalled
     set uninstall no
@@ -61,6 +97,15 @@ foreach port [registry::entry imaged] {
             # The version in the index is different than the installed one
             ui_msg "Removing ${installed_name} @${installed_version}_${installed_revision}${installed_variants} because there is a newer version in the PortIndex"
             set uninstall yes
+        } else {
+            set lowercase_name [string tolower $installed_name]
+            if {![info exists dependents($lowercase_name)]} {
+                # Nothing depends on it
+                ui_msg "Removing ${installed_name} @${installed_version}_${installed_revision}${installed_variants} because no port in the PortIndex depends on it"
+                set uninstall yes
+            } elseif {no} {
+                ui_msg "Not removing ${installed_name} @${installed_version}_${installed_revision}${installed_variants} because it has $dependents($lowercase_name) dependents"
+            }
         }
     }
     if {$uninstall} {
