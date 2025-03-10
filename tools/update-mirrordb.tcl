4b613b082c8c7bcce1acdddee11b875eb4f6ebd9
Joshua Root <jmr@macports.org>
2025-03-11 10:59:33 +1100

    mirror-multi: add remote db support

---
+++ tools/update-mirrordb.tcl
@@ -0,0 +1,24 @@
+#!/usr/bin/env port-tclsh
+
+# args:
+# mirrorcache_baseurl mirrorcache_credentials
+# then paths to three files containing tcl dicts:
+# mirror_done portfile_hash_cache portname_portfile_map
+
+package require Pextlib
+
+lassign $argv mirrorcache_baseurl mirrorcache_credentials mirror_done_p portfile_hash_cache_p portname_portfile_map_p
+foreach var {mirror_done portfile_hash_cache portname_portfile_map} {
+    set fd [open [set ${var}_p] r]
+    set $var [gets $fd]
+    close $fd
+}
+
+dict for {portname status} $mirror_done {
+    set portfile [dict get $portname_portfile_map $portname]
+    set hash [dict get $portfile_hash_cache $portfile]
+    set hashurl ${mirrorcache_baseurl}SET/mirror.sha256.${portname}/${hash}
+    set statusurl ${mirrorcache_baseurl}SET/mirror.status.${portname}/${status}
+    curl fetch -u $mirrorcache_credentials $hashurl /dev/null
+    curl fetch -u $mirrorcache_credentials $statusurl /dev/null
+}
